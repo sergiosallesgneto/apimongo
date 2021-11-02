911da32237ff294e55757fbdb6c654ce746e5ff6
sergiosallesgneto <88805252+sergiosallesgneto@users.noreply.github.com>
2021-11-02 16:21:58 -0300

    commit

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const app = express();
+const {MongoClient} = require("mongodb");
 
-app.get("/aluno", (req, res )=> {
+app.get("/alunos", (req, res )=> {
+    const cliente = new  MongoClient("mongodb://192.168.56.1:27017");
+    cliente.connect();
+    cliente.db("Fatec").collection("Alunos").find()
+    .toArray().then( (alunos) => {
+        res.send(alunos)
+    }    )
+
+/*
+app.get("/aluno", (req, res )=> { //endpoint /aluno
     let usuario = {
                     nome: "Claudio Nunes",
                     disciplina : "Topicos Especiais",
                     curso : "ADS"
     }
-    res.send(usuario)
+    res.send(usuario)*/
 }) ;
 
-app.get("/professores", (req, res )=> {
+app.get("/professores", (req, res )=> { //endpoint /professores
     let professores  = [ {
                     nome: "David Reis",
                     disciplina : "Topicos Especiais",
